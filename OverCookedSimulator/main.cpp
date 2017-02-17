e2ca8ffe585933c1b0a0b9b428476a1f8b0faf90
Willy-Wonka <willwcc@gmail.com>
2017-02-17 13:05:19 -0800

    Initial Commit

---
+++ OverCookedSimulator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OverCookedSimulator
+//
+//  Created by Will on 2017-02-17.
+//  Copyright © 2017 Muscle Ye. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
