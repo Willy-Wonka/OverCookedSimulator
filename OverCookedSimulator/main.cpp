8dacf68c64a51543743265f14433cb9e69d1e03a
Willy-Wonka <willwcc@gmail.com>
2017-02-17 17:23:30 -0800

    finished countdown timer & got all the actions time info

--- OverCookedSimulator/main.cpp
+++ OverCookedSimulator/main.cpp
@@ -7,17 +7,68 @@
 //
 
 #include <iostream>
-#include <math.h>
-
+#include <cmath>
+#include <thread>
 using namespace std;
+
+void countdown(int seconds)
+{
+	clock_t start = clock();
+	double duration = 0;
+	int timer = 0;
+	
+	while (duration < seconds)
+	{
+		duration = (clock() - start) / (double) CLOCKS_PER_SEC;
+		double fractional, integer;
+		fractional = modf(duration, &integer);	// get the integer part
+		
+		// if pass one whole second
+		if (timer == integer)
+		{
+			int secondsLeft = seconds - timer;
+			int minutesLeft = secondsLeft / 60;
+			secondsLeft -= minutesLeft * 60;
+			
+			// print out time left in 00:00 form
+			string timeLeft = "";
+			if (minutesLeft < 10)
+				timeLeft += "0";
+			timeLeft += to_string(minutesLeft) + ":";
+			if (secondsLeft < 10)
+				timeLeft += "0";
+			timeLeft += to_string(secondsLeft) + "\n";
+			
+			cout << timeLeft;
+			
+			timer++;
+		}
+	}
+}
 
 int main(int argc, const char * argv[])
 {
-	double x = 31415.9265, fractional, integer;
-	fractional = modf(x, &integer);
-	printf("%.4lf = %.4lf + %.4lf\n", x, integer, fractional);
+	int const CITY1_TIMER = 300;
+//	int const CUT_TOMATO = 8;
+//	int const CUT_CABBAGE = 8;
+//	int const CUT_BEEF = 8;
+//	int const FRIED_BEEF = 21;
+//	int const DELIVER_TIME = 1;
+//	int const DINING_TIME = 10;
+//	int const WASH_PLATE = 3;
+	int const HAMBURGER_TIME_LIMIT = 2 * 60 + 30;	// 37.5 * 4
+	
+	thread city1(countdown, CITY1_TIMER);
 		
 	// insert code here...
-	cout << "Hello, World!\n";
+	cout << "Hello, \n";
+	
+	for (int i = 0; i < 500; i++) {
+		cout << i << endl;
+	}
+	
+	city1.join();
+	cout << "World!\n";
+	
     return 0;
 }
